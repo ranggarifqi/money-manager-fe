d8d950bba0aa2da0ce487decc149fe31639b14b6
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-03 22:28:34 +0700

    Added base login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Main from "./pages/main/Main";
+import Login from "./pages/login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
