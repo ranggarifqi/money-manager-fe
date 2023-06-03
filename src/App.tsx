653ece27ee9bd28e842ea40a1e367cb4ce498249
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-03 22:30:52 +0700

    extract router to its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Main from "./pages/main/Main";
-import Login from "./pages/login/Login";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 function App() {
   return <RouterProvider router={router} />;
