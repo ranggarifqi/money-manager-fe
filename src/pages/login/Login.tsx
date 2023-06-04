002850c39a26b8fc1d536fde2abcd15c14cf758d
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-04 12:57:17 +0700

    Added login svg

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,6 +3,8 @@ import { initTE, Input } from "tw-elements";
 import Spacer from "../../commons/components/Spacer";
 import RippleButton from "../../commons/components/Button";
 
+import loginSVG from "/login.svg";
+
 const Login = () => {
   useEffect(() => {
     initTE({ Input });
@@ -10,54 +12,48 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center h-screen bg-main">
-      <div className="container bg-light-shades h-auto">
-        <div className="py-8 flex h-full flex-wrap items-center justify-center lg:justify-between bg-light-shades">
-          <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
-            <img
-              src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-              className="w-full"
-              alt="Sample image"
-            />
-          </div>
-          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12 p-8">
-            <form>
-              <div className="flex flex-row items-center justify-center lg:justify-start">
-                <h1 className="mb-0 mr-4">Welcome</h1>
-              </div>
-              <Spacer height={20} />
-              <div className="relative mb-6" data-te-input-wrapper-init>
-                <input
-                  type="text"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlInput2"
-                  placeholder="Email address"
-                />
-                <label
-                  htmlFor="exampleFormControlInput2"
-                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                >
-                  Email address
-                </label>
-              </div>
+      <div className="container bg-light-shades py-8 flex flex-wrap items-center justify-center lg:justify-between bg-light-shades min-h-[500px]">
+        <div className="shrink-1 grow-0 basis-auto md:mb-0 md:w-6/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+          <img src={loginSVG} className="w-full" alt="Login" />
+        </div>
+        <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12 p-8">
+          <form>
+            <div className="flex flex-row items-center justify-center lg:justify-start">
+              <h1 className="mb-0 mr-4">Welcome</h1>
+            </div>
+            <Spacer height={20} />
+            <div className="relative mb-6" data-te-input-wrapper-init>
+              <input
+                type="text"
+                className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="exampleFormControlInput2"
+                placeholder="Email address"
+              />
+              <label
+                htmlFor="exampleFormControlInput2"
+                className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+              >
+                Email address
+              </label>
+            </div>
 
-              <div className="relative mb-6" data-te-input-wrapper-init>
-                <input
-                  type="password"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlInput22"
-                  placeholder="Password"
-                />
-                <label
-                  htmlFor="exampleFormControlInput22"
-                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                >
-                  Password
-                </label>
-              </div>
+            <div className="relative mb-6" data-te-input-wrapper-init>
+              <input
+                type="password"
+                className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="exampleFormControlInput22"
+                placeholder="Password"
+              />
+              <label
+                htmlFor="exampleFormControlInput22"
+                className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+              >
+                Password
+              </label>
+            </div>
 
-              <RippleButton block>Login</RippleButton>
-            </form>
-          </div>
+            <RippleButton block>Login</RippleButton>
+          </form>
         </div>
       </div>
     </div>
