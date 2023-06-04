a7f95421ab02e774ec5c140483b75e15b7f49369
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-04 17:47:30 +0700

    Installed react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import queryClient from "./api/query";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
