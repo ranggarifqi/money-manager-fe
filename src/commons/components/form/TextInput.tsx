218ea12039df834d65aaf62bdb0d20694619fb0b
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-04 17:05:12 +0700

    Added react hook form & validation

--- src/commons/components/form/TextInput.tsx
+++ src/commons/components/form/TextInput.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import React from "react";
 import { useEffect } from "react";
 import { initTE, Input } from "tw-elements";
 
@@ -13,32 +14,41 @@ type Props = OwnProps &
     HTMLInputElement
   >;
 
-const TextInput = ({ label = "Label", errorText, ...inputProps }: Props) => {
-  useEffect(() => {
-    initTE({ Input });
-  }, []);
+const TextInput = React.forwardRef<HTMLInputElement, Props>(
+  ({ errorText, label, onChange, onBlur, name }: Props, ref) => {
+    useEffect(() => {
+      initTE({ Input });
+    }, []);
 
-  const inputClassNames = classNames(defaultInputClassNames, {
-    "ring-1 ring-danger outline-0": !!errorText,
-    "focus:outline-none focus:border-main focus:ring-1 focus:ring-main":
-      !errorText,
-  });
+    const inputClassNames = classNames(defaultInputClassNames, {
+      "ring-1 ring-danger outline-0": !!errorText,
+      "focus:outline-none focus:border-main focus:ring-1 focus:ring-main":
+        !errorText,
+    });
 
-  const labelClassName = classNames(
-    "block text-sm font-medium text-dark-shades",
-    {
-      "text-danger": !!errorText,
-    }
-  );
+    const labelClassName = classNames(
+      "block text-sm font-medium text-dark-shades",
+      {
+        "text-danger": !!errorText,
+      }
+    );
 
-  return (
-    <label className="block">
-      <span className={labelClassName}>{label}</span>
-      <input type="text" className={inputClassNames} {...inputProps} />
-      {errorText && <span className="text-sm text-danger">{errorText}</span>}
-    </label>
-  );
-};
+    return (
+      <label className="block">
+        <span className={labelClassName}>{label}</span>
+        <input
+          ref={ref}
+          type="text"
+          className={inputClassNames}
+          name={name}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        {errorText && <span className="text-sm text-danger">{errorText}</span>}
+      </label>
+    );
+  }
+);
 
 const defaultInputClassNames = classNames(
   "mt-1",
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,8 +3,21 @@ import RippleButton from "../../commons/components/RippleButton";
 
 import loginSVG from "/login.svg";
 import TextInput from "../../commons/components/form/TextInput";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-br from-main from-40% to-main-50">
       <div className="container h-full max-h-[500px] bg-light-shades flex flex-col sm:flex-row md:flex-wrap items-center justify-start">
@@ -12,14 +25,24 @@ const Login = () => {
           <img src={loginSVG} className="w-full" alt="Login" />
         </div>
         <div className="flex-1 p-10 bg-white h-full flex items-center">
-          <form>
+          <form onSubmit={onSubmit}>
             <h1 className="mb-0 mr-4">Welcome! Please Login to Continue</h1>
             <Spacer height={20} />
-            <TextInput label="Email Address" />
+            <TextInput
+              label="Email Address"
+              {...register("email", { required: "Email Address is required" })}
+              errorText={errors.email?.message ?? undefined}
+            />
             <Spacer height={10} />
-            <TextInput label="Password" />
+            <TextInput
+              label="Password"
+              {...register("password", { required: "Password is required" })}
+              errorText={errors.password?.message ?? undefined}
+            />
             <Spacer height={20} />
-            <RippleButton block>Login</RippleButton>
+            <RippleButton block type="submit">
+              Login
+            </RippleButton>
           </form>
         </div>
       </div>
