a337b21f814e77b7358885f750913f1093a811cd
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-06 22:33:02 +0700

    Fixed import

--- src/store/session/slice.ts
+++ src/store/session/slice.ts
@@ -1,6 +1,5 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import jwtDecode from "jwt-decode";
-import { sessionAPI } from "./api";
 
 interface IUserCredential {
   userId: string;
