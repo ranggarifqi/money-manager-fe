9cc93e8451f6b353e764745ffbf9ae6e312238ba
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-08 23:15:17 +0700

    Make SideNavMenuItem reusable

--- src/pages/main/components/SideNav/SideNavMenu.tsx
+++ src/pages/main/components/SideNav/SideNavMenu.tsx
@@ -1,9 +1,10 @@
+import { HomeIcon } from "@heroicons/react/24/outline";
 import SideNavMenuItem from "./SideNavMenuItem";
 
 const SideNavMenu = () => {
   return (
     <ul className="relative m-0 list-none px-[0.2rem]" data-te-sidenav-menu-ref>
-      <SideNavMenuItem />
+      <SideNavMenuItem label="Dashboard" redirectTo="/" Icon={HomeIcon} />
     </ul>
   );
 };
