97600f3a5c1e9158de56583de03305ebd878cc47
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-08 22:13:08 +0700

    Handle logout

--- src/pages/main/components/Header/AvatarMenu.tsx
+++ src/pages/main/components/Header/AvatarMenu.tsx
@@ -1,9 +1,17 @@
 import { Menu, MenuItem } from "@blueprintjs/core";
+import { useAppDispatch } from "../../../../commons/hooks/useAppDispatch";
+import { logoutSuccess } from "../../../../store/session/slice";
 
 const AvatarMenu = () => {
+  const dispatch = useAppDispatch();
+
+  const onLogout = () => {
+    dispatch(logoutSuccess());
+  };
+
   return (
     <Menu>
-      <MenuItem icon="log-out" onClick={undefined} text="Logout" />
+      <MenuItem icon="log-out" onClick={onLogout} text="Logout" />
     </Menu>
   );
 };
