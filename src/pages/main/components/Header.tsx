e86fed9c2aff0f81a902e362af5b59623fae6eca
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-08 22:01:17 +0700

    Added blueprintjs

--- src/pages/main/components/Header.tsx
+++ src/pages/main/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Bars3Icon } from "@heroicons/react/20/solid";
+import { Popover2 } from "@blueprintjs/popover2";
 import { useEffect } from "react";
 import { Collapse, Dropdown, initTE } from "tw-elements";
 
@@ -14,7 +14,7 @@ const Header = () => {
     >
       <div className="flex w-full flex-wrap items-center justify-between px-3">
         <button
-          className="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
+          className="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200"
           type="button"
           data-te-collapse-init
           data-te-target="#navbarSupportedContent1"
@@ -38,66 +38,10 @@ const Header = () => {
           </span>
         </button>
 
-        <div
-          className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
-          id="navbarSupportedContent1"
-          data-te-collapse-item
-        >
-          <Bars3Icon className="h-6 w-6 cursor-pointer" />
-        </div>
-
         <div className="relative flex items-center">
-          <div className="relative" data-te-dropdown-ref>
-            <a
-              className="hidden-arrow flex items-center whitespace-nowrap transition duration-150 ease-in-out motion-reduce:transition-none"
-              href="#"
-              id="dropdownMenuButton2"
-              role="button"
-              data-te-dropdown-toggle-ref
-              aria-expanded="false"
-            >
-              <img
-                src="https://tecdn.b-cdn.net/img/new/avatars/2.jpg"
-                className="rounded-full"
-                style={{ height: "25px", width: "25px" }}
-                alt=""
-                loading="lazy"
-              />
-            </a>
-            <ul
-              className="absolute left-auto right-0 z-[1000] float-left m-0 mt-1 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-              aria-labelledby="dropdownMenuButton2"
-              data-te-dropdown-menu-ref
-            >
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                >
-                  Action
-                </a>
-              </li>
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                >
-                  Another action
-                </a>
-              </li>
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                >
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </div>
+          <Popover2 content={<div>asd</div>}>
+            <div className="rounded-full h-8 w-8 bg-main"></div>
+          </Popover2>
         </div>
       </div>
     </nav>
