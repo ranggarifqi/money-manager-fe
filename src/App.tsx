65ada8decbe8fb5e48081a387d971a8f3477a324
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-10 11:33:43 +0700

    Removed react-query

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import router from "./router";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import queryClient from "./api/query";
 import { persistor, store } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -12,10 +9,7 @@ function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        <RouterProvider router={router} />
       </PersistGate>
     </Provider>
   );
--- src/api/fn/refresh.ts
+++
@@ -1,9 +0,0 @@
-import { SuccessResponse, getAxiosInstance } from "../axios";
-
-export const refresh = async () => {
-  const api = getAxiosInstance();
-  const { data } = await api.get<SuccessResponse<string>>("/auth/refresh");
-
-  const { data: accessToken } = data;
-  return accessToken;
-};
