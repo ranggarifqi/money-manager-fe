1ba648b0f437ddce0b495fc5f838c7220477894a
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-11 13:37:30 +0700

    Added edit & delete menu

--- src/pages/accounts/commons/AccountBalance.tsx
+++ src/pages/accounts/commons/AccountBalance.tsx
@@ -2,6 +2,7 @@ import { useTheme } from "styled-components";
 import Card from "../../../commons/components/Card";
 import { Popover2 } from "@blueprintjs/popover2";
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
+import AccountMenu from "./AccountMenu";
 
 const AccountBalance = () => {
   const theme = useTheme();
@@ -13,7 +14,7 @@ const AccountBalance = () => {
         <h3>Rp 1000,00</h3>
       </div>
       <div className="flex-none relative top-[-0.3rem]">
-        <Popover2 content={<p>sadadasd</p>}>
+        <Popover2 content={<AccountMenu />}>
           <EllipsisHorizontalIcon className="w-8 h-8 text-white cursor-pointer" />
         </Popover2>
       </div>
