a63cd603c6fb8a902c60790fe647afd508ce8f31
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-11 14:18:54 +0700

    Added onClick to Card

--- src/commons/components/Card.tsx
+++ src/commons/components/Card.tsx
@@ -1,19 +1,26 @@
 import classNames from "classnames";
 import { ReactNode } from "react";
 import { styled } from "styled-components";
+import { AnyCallback } from "../types/general";
 
 interface Props {
   className?: string;
   children?: ReactNode;
   width?: string;
   bgColor?: string;
+  onClick?: AnyCallback;
 }
 
-const Card = ({ children, width, bgColor, className }: Props) => {
+const Card = ({ children, width, bgColor, className, onClick }: Props) => {
   const concattedClassName = classNames("rounded-lg p-4", className);
 
   return (
-    <Container className={concattedClassName} width={width} bgColor={bgColor}>
+    <Container
+      className={concattedClassName}
+      width={width}
+      bgColor={bgColor}
+      onClick={onClick}
+    >
       {children}
     </Container>
   );
