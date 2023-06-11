0f1915928c4fda7832b0759edd7dc305012ac81b
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-11 13:29:18 +0700

    Added 3 elipsis icon

--- src/pages/accounts/commons/AccountCard.tsx
+++ src/pages/accounts/commons/AccountCard.tsx
@@ -3,6 +3,7 @@ import Card from "../../../commons/components/Card";
 import Spacer from "../../../commons/components/Spacer";
 import RecentTransaction from "./RecentTransaction";
 import { Link } from "react-router-dom";
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 
 interface Props {
   title: string;
@@ -20,14 +21,21 @@ const AccountCard = ({ title }: Props) => {
           </div>
         </div>
         <div className="w-full mt-2">
-          <Card bgColor={theme?.colors.main[800]} className="text-white">
-            <p>Balance</p>
-            <h3>Rp 1000,00</h3>
+          <Card bgColor={theme?.colors.main[800]} className="text-white flex">
+            <div className="flex-1">
+              <p>Balance</p>
+              <h3>Rp 1000,00</h3>
+            </div>
+            <div className="flex-none relative top-[-0.3rem]">
+              <EllipsisHorizontalIcon className="w-8 h-8 text-white cursor-pointer" />
+            </div>
           </Card>
           <Spacer height={20} />
           <RecentTransaction />
-          <Spacer height={20}/>
-          <Link to='/accounts/id/transactions' className="text-right block">See All Transactions</Link>
+          <Spacer height={20} />
+          <Link to="/accounts/id/transactions" className="text-right block">
+            See All Transactions
+          </Link>
         </div>
       </div>
     </Card>
