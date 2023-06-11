29befd6f7ca1cfe233cb60dcc55282ca6db23b2d
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-11 11:19:21 +0700

    Added custom className to Card

--- src/commons/components/Card.tsx
+++ src/commons/components/Card.tsx
@@ -1,15 +1,19 @@
+import classNames from "classnames";
 import { ReactNode } from "react";
 import { styled } from "styled-components";
 
 interface Props {
+  className?: string;
   children?: ReactNode;
   width?: string;
   bgColor?: string;
 }
 
-const Card = ({ children, width, bgColor }: Props) => {
+const Card = ({ children, width, bgColor, className }: Props) => {
+  const concattedClassName = classNames("rounded-lg p-4", className);
+
   return (
-    <Container className="rounded-lg p-4" width={width} bgColor={bgColor}>
+    <Container className={concattedClassName} width={width} bgColor={bgColor}>
       {children}
     </Container>
   );
--- src/pages/accounts/commons/AccountCard.tsx
+++ src/pages/accounts/commons/AccountCard.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 const AccountCard = ({ title }: Props) => {
   return (
-    <Card>
-      <div className="flex flex-col items-center relative h-36">
+    <Card className="min-h-[10rem]">
+      <div className="flex flex-col items-center">
         <div className="relative top-[-2rem] w-3/4 h-6">
           <div className="h-12 bg-main rounded-lg flex items-center justify-center px-3">
             <h2 className="text-light-shades truncate">{title}</h2>
