5c39846b1a1dda0c55f6dfc1c5df76115da2f072
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-12 21:20:41 +0700

    Show SelectField

--- src/pages/accounts/NewAccount.tsx
+++ src/pages/accounts/NewAccount.tsx
@@ -3,6 +3,17 @@ import Card from "../../commons/components/Card";
 import Spacer from "../../commons/components/Spacer";
 import TextInput from "../../commons/components/form/TextInput";
 import { usePageTitle } from "../../commons/hooks/usePageTitle";
+import SelectField, {
+  SelectOption,
+} from "../../commons/components/form/SelectField";
+import { EAccountType } from "../../commons/models/accountType";
+
+const options: SelectOption[] = Object.values(EAccountType).map((v) => {
+  return {
+    label: v,
+    value: v,
+  };
+});
 
 const NewAccount = () => {
   usePageTitle({
@@ -24,6 +35,8 @@ const NewAccount = () => {
     <div className="flex justify-center">
       <Card width="50%">
         <form onSubmit={onSubmit}>
+          <SelectField label="Account Type" options={options} />
+          <Spacer height={15} />
           <TextInput label="Account Name" {...register("name")} />
           <Spacer height={15} />
           <TextInput label="Account Balance" {...register("balance")} />
