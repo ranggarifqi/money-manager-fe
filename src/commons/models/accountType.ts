7f5cc23227b514ccac0f66a371d04f22883fd890
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-12 21:20:27 +0700

    Added EAccountType

---
+++ src/commons/models/accountType.ts
@@ -0,0 +1,10 @@
+export enum EAccountType {
+  RECEIVABLES = "Receivables",
+  SAVINGS = "Savings",
+  INVESTMENTS = "Investments",
+  CASH = "Cash",
+  LOANS = "Loans",
+  PREPAID = "Prepaid",
+  OTHERS = "Others",
+  OVERDRAFTS = "Overdrafts",
+}
