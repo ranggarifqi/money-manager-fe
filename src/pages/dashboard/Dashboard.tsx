ff9e56433c69ab859cfa18713ab0cb1188d3aecd
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-17 10:31:06 +0700

    Updated Dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,6 @@
-import RippleButton from "../../commons/components/RippleButton";
-import { useFindAccountsMutation } from "../../store/account/api";
 import { usePageTitle } from "../../commons/hooks/usePageTitle";
 
 const Dashboard = () => {
-  const [fetchAccount] = useFindAccountsMutation();
   usePageTitle({
     title: "Dashboard",
     breadcrumb: ["Dashboard"],
@@ -11,7 +8,7 @@ const Dashboard = () => {
 
   return (
     <div>
-      <RippleButton onClick={() => fetchAccount()}>Fetch account</RippleButton>
+      <p>Dashboard</p>
     </div>
   );
 };
