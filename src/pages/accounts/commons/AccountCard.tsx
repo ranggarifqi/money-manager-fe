f81a17705c3b5161bbfba70f9f8b41a2c9a57a93
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-17 13:40:16 +0700

    Show recent transaction

--- src/pages/accounts/commons/AccountCard.tsx
+++ src/pages/accounts/commons/AccountCard.tsx
@@ -14,7 +14,7 @@ const AccountCard = ({ accountId }: Props) => {
   const account = useAppSelector((state) => sltAccountById(state, accountId));
   return (
     <Card>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center h-full">
         <div className="relative top-[-2.5rem] w-3/4">
           <div className="py-2 h-16 bg-main rounded-lg flex flex-col items-center justify-center px-3">
             <h2 className="text-light-shades truncate">{account?.name}</h2>
@@ -25,10 +25,10 @@ const AccountCard = ({ accountId }: Props) => {
             )}
           </div>
         </div>
-        <div className="w-full mt-[-1.5rem]">
+        <div className="relative w-full mt-[-1.5rem] h-full flex flex-col">
           <AccountBalance accountId={accountId} />
           <Spacer height={20} />
-          <RecentTransaction />
+          <RecentTransaction accountId={accountId} />
           <Spacer height={20} />
           <Link to="/accounts/id/transactions" className="text-right block">
             See All Transactions
--- src/pages/accounts/commons/RecentTransaction.tsx
+++ src/pages/accounts/commons/RecentTransaction.tsx
@@ -1,16 +1,36 @@
 import Spacer from "../../../commons/components/Spacer";
+import Spinner from "../../../commons/components/Spinner";
+import { useFindTransactionByAccountQuery } from "../../../store/transaction/api";
 import RecentTransactionItem from "./RecentTransactionItem";
 
-const RecentTransaction = () => {
+interface Props {
+  accountId: string;
+}
+
+const RecentTransaction = ({ accountId }: Props) => {
+  const { isFetching, data } = useFindTransactionByAccountQuery({
+    accountId,
+    limit: 3,
+  });
+
+  if (isFetching) {
+    return <Spinner />;
+  }
+
+  const renderData = () => {
+    if (!data || data.length === 0) {
+      return <p className="text-xs">No Transaction found.</p>;
+    }
+    return data?.map(() => {
+      return <RecentTransactionItem />;
+    });
+  };
+
   return (
-    <div>
+    <div className="flex-1">
       <p className="text-light-accent">Recent Transactions</p>
       <Spacer height={6} />
-      <ul className="flex flex-col gap-y-2">
-        <RecentTransactionItem />
-        <RecentTransactionItem />
-        <RecentTransactionItem />
-      </ul>
+      <ul className="flex flex-col gap-y-2">{renderData()}</ul>
     </div>
   );
 };
