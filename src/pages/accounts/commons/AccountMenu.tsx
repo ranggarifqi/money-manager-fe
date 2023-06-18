2ebec1d9b2440d7232ba49da917c1c4edd5a3d65
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-18 11:33:25 +0700

    Pass in account name to the modal

--- src/pages/accounts/commons/AccountMenu.tsx
+++ src/pages/accounts/commons/AccountMenu.tsx
@@ -3,6 +3,9 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { showModal } from "../../../store/modal/slice";
 import { ModalType } from "../../../commons/components/modals/interfaces";
+import { AccountDeletionConfirmationProps } from "../../../commons/components/modals/types/AccountDeletionConfirmationModal";
+import { useAppSelector } from "../../../commons/hooks/useAppSelector";
+import { sltAccountById } from "../../../store/account/selectors";
 
 interface Props {
   accountId: string;
@@ -11,6 +14,9 @@ interface Props {
 const AccountMenu = ({ accountId }: Props) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const account = useAppSelector((state) => sltAccountById(state, accountId));
+
   return (
     <Menu>
       <MenuItem
@@ -26,7 +32,8 @@ const AccountMenu = ({ accountId }: Props) => {
               modalName: ModalType.ACCOUNT_DELETION_CONFIRMATION,
               props: {
                 accountId,
-              },
+                accountName: account?.name,
+              } as AccountDeletionConfirmationProps,
             })
           )
         }
