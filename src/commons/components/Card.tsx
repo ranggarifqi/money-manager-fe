51bdbde71e0d475b1d7738f31893f2a1bf213d9f
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-24 08:56:17 +0700

    Use styled component on Card

--- src/commons/components/Card.tsx
+++ src/commons/components/Card.tsx
@@ -1,20 +1,40 @@
 import classNames from "classnames";
 import { ReactNode } from "react";
 import { AnyCallback } from "../types/general";
+import { styled } from "styled-components";
 
 interface Props {
   className?: string;
   children?: ReactNode;
   onClick?: AnyCallback;
+  bgColor?: string;
 }
 
-const Card = ({ children, className, onClick }: Props) => {
-  const concattedClassName = classNames("rounded-lg p-4 w-full bg-light-shades", className);
+/**
+ * 
+ * Note:
+ *  * Don't use tailwind's bg- class.
+ */
+const Card = ({ children, className, onClick, bgColor }: Props) => {
+  const concattedClassName = classNames(className);
 
   return (
-    <div className={concattedClassName} onClick={onClick}>
+    <Container
+      className={concattedClassName}
+      onClick={onClick}
+      $bgColor={bgColor}
+    >
       {children}
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div<{ $bgColor?: string }>`
+  border-radius: 0.5rem;
+  padding: 1rem;
+  width: 100%;
+  background-color: ${(props) =>
+    props.$bgColor ?? props.theme.colors["light-shades"]};
+`;
+
 export default Card;
--- src/pages/accounts/commons/AddAccountButton.tsx
+++ src/pages/accounts/commons/AddAccountButton.tsx
@@ -7,8 +7,9 @@ const AddAccountCard = () => {
 
   return (
     <Card
-      className="bg-transparent border-4 border-dark-accent border-dotted text-dark-accent hover:text-light-accent flex flex-col items-center justify-center hover:border-light-accent cursor-pointer"
+      className="border-4 border-dark-accent border-dotted text-dark-accent hover:text-light-accent flex flex-col items-center justify-center hover:border-light-accent cursor-pointer"
       onClick={() => navigate("/accounts/new")}
+      bgColor="rgb(0,0,0,0)"
     >
       <PlusCircleIcon className="w-[6rem] h-[6rem] " />
       <p className="text-lg">Add new account</p>
