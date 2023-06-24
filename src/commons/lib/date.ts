e3c1c4d509c766625f2c7386aeb49d39741767e7
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-24 16:06:42 +0700

    Added date formats

--- src/commons/lib/date.ts
+++ src/commons/lib/date.ts
@@ -1,5 +1,8 @@
 import * as dateFns from "date-fns";
 
+export const HUMANIZED_DATE = "d MMM yyyy";
+export const DATE_ONLY_KEY = 'yyyy-MM-dd'
+
 export const toHumanReadableDate = (date: string) => {
   return dateFns.format(dateFns.parseISO(date), "d MMM yyyy");
 };
