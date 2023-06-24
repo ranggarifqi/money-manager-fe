84ba6c9cc09d737689dd5db951295134ef1e0600
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-24 17:20:47 +0700

    Refactor & Load Data

--- src/pages/accounts/commons/TransactionList/AccountTransactionList.tsx
+++ src/pages/accounts/commons/TransactionList/AccountTransactionList.tsx
@@ -1,23 +1,13 @@
 import { groupBy } from "lodash-es";
-import { styled } from "styled-components";
-
-import Card from "../../../../commons/components/Card";
 import { ITransactionWithAssociation } from "../../../../commons/models/transaction";
-import {
-  getDayFromDateStr,
-  getMonthFromDateStr,
-  getYearFromDateStr,
-  toDateOnlyString,
-} from "../../../../commons/lib/date";
-import { useCurrencyFormatter } from "../../../../commons/hooks/useCurrencyFormatter";
+import { toDateOnlyString } from "../../../../commons/lib/date";
+import AccountTransactionListByDate from "./AccountTransactionListByDate";
 
 interface Props {
   data?: ITransactionWithAssociation[];
 }
 
 const AccountTransactionList = ({ data = [] }: Props) => {
-  const { formatBalance } = useCurrencyFormatter();
-
   const hasNoData = data.length === 0;
 
   if (hasNoData) {
@@ -38,82 +28,16 @@ const AccountTransactionList = ({ data = [] }: Props) => {
       {dateList.map((dateStr) => {
         const transactions = transactionsGroupedByDate[dateStr] ?? [];
 
-        const totalDeposit = transactions.reduce((res, v) => {
-          if (v.toAccountId !== null) {
-            return res + v.amount;
-          }
-          return res;
-        }, 0);
-
-        const totalWithdrawal = transactions.reduce((res, v) => {
-          if (v.fromAccountId !== null) {
-            return res + v.amount;
-          }
-          return res;
-        }, 0);
-
         return (
-          <DayCard bgColor="white" key={dateStr}>
-            <div className="flex border-b-2 py-2 px-4 gap-x-2 items-center">
-              <span className="flex-1">
-                <span className="text-lg">{getDayFromDateStr(dateStr)}</span>{" "}
-                <span className="text-xs">
-                  / {getMonthFromDateStr(dateStr)} /{" "}
-                  {getYearFromDateStr(dateStr)}
-                </span>
-              </span>
-              <span className="flex-1 text-success text-right">
-                {formatBalance(totalDeposit)}
-              </span>
-              <span className="flex-1 text-danger text-right">
-                {formatBalance(totalWithdrawal)}
-              </span>
-            </div>
-            <div className="flex flex-col divide-y">
-              <div className="px-4 py-2 flex w-full gap-x-4">
-                <div className="flex-0 w-[5rem]">
-                  <p className="truncate">Category</p>
-                  <p className="text-xs truncate">Sub Category</p>
-                </div>
-                <div className="flex-0">
-                  <p>Transaction</p>
-                  <p className="text-xs">Account Name</p>
-                </div>
-                <div className="flex-1 text-right">Rp 0</div>
-              </div>
-              <div className="px-4 py-2 flex w-full gap-x-4">
-                <div className="flex-0 w-[5rem]">
-                  <p className="truncate">Category</p>
-                  <p className="text-xs truncate">Sub Category</p>
-                </div>
-                <div className="flex-0">
-                  <p>Transaction</p>
-                  <p className="text-xs">Account Name</p>
-                </div>
-                <div className="flex-1 text-right">Rp 0</div>
-              </div>
-              <div className="px-4 py-2 flex w-full gap-x-4">
-                <div className="flex-0 w-[5rem]">
-                  <p className="truncate">Category</p>
-                  <p className="text-xs truncate">Sub Category</p>
-                </div>
-                <div className="flex-0">
-                  <p>Transaction</p>
-                  <p className="text-xs">Account Name</p>
-                </div>
-                <div className="flex-1 text-right">Rp 0</div>
-              </div>
-            </div>
-          </DayCard>
+          <AccountTransactionListByDate
+            dateStr={dateStr}
+            key={dateStr}
+            transactions={transactions}
+          />
         );
       })}
     </div>
   );
 };
-
-const DayCard = styled(Card)`
-  padding: 0;
-  border-ra
-`;
 
 export default AccountTransactionList;
