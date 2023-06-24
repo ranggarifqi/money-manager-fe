5318dbc0bc4b4c2f4e85314c9ea3a231eb419abf
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-24 16:58:14 +0700

    Show total WIthdrawal & deposit for each date

--- src/pages/accounts/commons/TransactionList/AccountTransactionList.tsx
+++ src/pages/accounts/commons/TransactionList/AccountTransactionList.tsx
@@ -9,12 +9,15 @@ import {
   getYearFromDateStr,
   toDateOnlyString,
 } from "../../../../commons/lib/date";
+import { useCurrencyFormatter } from "../../../../commons/hooks/useCurrencyFormatter";
 
 interface Props {
   data?: ITransactionWithAssociation[];
 }
 
 const AccountTransactionList = ({ data = [] }: Props) => {
+  const { formatBalance } = useCurrencyFormatter();
+
   const hasNoData = data.length === 0;
 
   if (hasNoData) {
@@ -33,6 +36,22 @@ const AccountTransactionList = ({ data = [] }: Props) => {
   return (
     <div className="flex flex-col gap-y-2">
       {dateList.map((dateStr) => {
+        const transactions = transactionsGroupedByDate[dateStr] ?? [];
+
+        const totalDeposit = transactions.reduce((res, v) => {
+          if (v.toAccountId !== null) {
+            return res + v.amount;
+          }
+          return res;
+        }, 0);
+
+        const totalWithdrawal = transactions.reduce((res, v) => {
+          if (v.fromAccountId !== null) {
+            return res + v.amount;
+          }
+          return res;
+        }, 0);
+
         return (
           <DayCard bgColor="white" key={dateStr}>
             <div className="flex border-b-2 py-2 px-4 gap-x-2 items-center">
@@ -43,8 +62,12 @@ const AccountTransactionList = ({ data = [] }: Props) => {
                   {getYearFromDateStr(dateStr)}
                 </span>
               </span>
-              <span className="flex-1 text-success text-right">Rp 0</span>
-              <span className="flex-1 text-danger text-right">Rp 0</span>
+              <span className="flex-1 text-success text-right">
+                {formatBalance(totalDeposit)}
+              </span>
+              <span className="flex-1 text-danger text-right">
+                {formatBalance(totalWithdrawal)}
+              </span>
             </div>
             <div className="flex flex-col divide-y">
               <div className="px-4 py-2 flex w-full gap-x-4">
