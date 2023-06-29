fc7330daff06cb260b500b96e1222a30f2bacb4f
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-29 13:00:21 +0700

    Added Category Type Field

--- src/pages/categories/commons/CategoryForm.tsx
+++ src/pages/categories/commons/CategoryForm.tsx
@@ -1,11 +1,23 @@
+import Spacer from "../../../commons/components/Spacer";
+import SelectField, {
+  SelectOption,
+} from "../../../commons/components/form/SelectField";
 import TextInput from "../../../commons/components/form/TextInput";
+import { ETransactionType } from "../../../commons/models/transactionType";
 
 const CategoryForm = () => {
   return (
     <form>
       <TextInput label="Category Name" />
+      <Spacer height={10} />
+      <SelectField label="Category Type" options={options} />
     </form>
   );
 };
+
+const options: SelectOption[] = [
+  { label: ETransactionType.EXPENSE, value: ETransactionType.EXPENSE },
+  { label: ETransactionType.INCOME, value: ETransactionType.INCOME },
+];
 
 export default CategoryForm;
