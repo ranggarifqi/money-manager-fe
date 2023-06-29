469afc86a8bb4db846c0670b3264c9cc92fd0b64
Rangga Rifqi <rangga@ranggarifqi.com>
2023-06-29 14:02:42 +0700

    Wireup form data

--- src/pages/categories/commons/CategoryForm.tsx
+++ src/pages/categories/commons/CategoryForm.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import * as yup from "yup";
+
 import Spacer from "../../../commons/components/Spacer";
 import SelectField, {
   SelectOption,
@@ -12,8 +14,29 @@ import {
   sltIncomeCategoryRaw,
 } from "../../../store/category/selectors";
 import Spinner from "../../../commons/components/Spinner";
+import { AnyCallback } from "../../../commons/types/general";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import RippleButton from "../../../commons/components/buttons/RippleButton";
 
-const CategoryForm = () => {
+const formSchema = yup
+  .object({
+    parentId: yup.string().required(),
+    name: yup.string().required(),
+    type: yup.string().required(),
+  })
+  .required();
+
+type FormData = yup.InferType<typeof formSchema>;
+
+interface Props {
+  onCancel?: AnyCallback;
+  onSubmit?: (data: FormData) => void;
+  submissionError?: string;
+  initialValues?: FormData;
+}
+
+const CategoryForm = ({ onCancel }: Props) => {
   const [parentOptions, setParentOptions] =
     useState<SelectOption[]>(defaultParentOptions);
 
@@ -22,11 +45,27 @@ const CategoryForm = () => {
 
   const { isSuccess, isLoading } = useFindCategoriesQuery();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm<FormData>({
+    resolver: yupResolver(formSchema),
+    defaultValues: {
+      type: ETransactionType.EXPENSE,
+      parentId: NONE,
+    },
+  });
+
+  const type = watch("type");
+
   useEffect(() => {
     if (isSuccess) {
       let additionalParentOptions: SelectOption[] = [];
 
-      if (defaultTypeOptions === ETransactionType.EXPENSE) {
+      if (type === ETransactionType.EXPENSE) {
         additionalParentOptions = allExpenseCategories.map<SelectOption>(
           (v) => ({ label: v.name, value: v.id })
         );
@@ -36,37 +75,60 @@ const CategoryForm = () => {
         );
       }
 
+      setValue("parentId", NONE);
       setParentOptions([...defaultParentOptions, ...additionalParentOptions]);
     }
-  }, [allExpenseCategories, allIncomeCategories, isSuccess]);
+  }, [allExpenseCategories, allIncomeCategories, isSuccess, type, setValue]);
+
+  const onSubmit = handleSubmit((data) => {
+    console.log({ data });
+  });
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       {isLoading ? (
         <Spinner />
       ) : (
-        <SelectField label="Parent Category" options={parentOptions} />
+        <SelectField
+          label="Parent Category"
+          options={parentOptions}
+          {...register("parentId")}
+          errorText={errors.parentId?.message}
+        />
       )}
 
       <Spacer height={10} />
-      <TextInput label="Category Name" />
+      <TextInput
+        label="Category Name"
+        {...register("name")}
+        errorText={errors.name?.message}
+      />
       <Spacer height={10} />
       <SelectField
         label="Category Type"
         options={typeOptions}
         initialValue={ETransactionType.EXPENSE}
+        {...register("type")}
+        errorText={errors.type?.message}
       />
+      <Spacer height={25} />
+      <div className="flex gap-x-3 w-full justify-end">
+        <RippleButton bgColor="info" type="button" onClick={onCancel}>
+          Cancel
+        </RippleButton>
+        <RippleButton type="submit">Save</RippleButton>
+      </div>
     </form>
   );
 };
-
-const defaultTypeOptions = ETransactionType.EXPENSE;
 
 const typeOptions: SelectOption[] = [
   { label: ETransactionType.EXPENSE, value: ETransactionType.EXPENSE },
   { label: ETransactionType.INCOME, value: ETransactionType.INCOME },
 ];
 
-const defaultParentOptions: SelectOption[] = [{ label: "None", value: "None" }];
+const NONE = "None";
+
+const defaultParentOptions: SelectOption[] = [{ label: NONE, value: NONE }];
 
 export default CategoryForm;
